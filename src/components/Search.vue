12373bbe835189c8863e9b7d690fcb4df934a11d
João <joaogcsoares1@gmail.com>
2021-01-22 22:14:28 -0300

    Adding expansion list and loading above the input

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,16 @@
 <template>
   <v-container>
+    <v-progress-linear
+      :active="true"
+      :indeterminate="true"
+      color="secondary"
+      rounded
+    ></v-progress-linear>
     <v-autocomplete
       v-model="model"
       :items="items"
-      :loading="isLoading"
       :search-input.sync="search"
-      class="mb-n7 "
+      class="mb-n7"
       hide-no-data
       hide-selected
       label="Search currencies"
@@ -21,6 +26,16 @@
       solo
       return-object
     ></v-autocomplete>
+    <v-expand-transition>
+      <v-list v-if="model" class="red lighten-3">
+        <v-list-item v-for="(field, i) in fields" :key="i">
+          <v-list-item-content>
+            <v-list-item-title v-text="field.value"></v-list-item-title>
+            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-expand-transition>
   </v-container>
 </template>
 
@@ -29,13 +44,69 @@ export default {
   name: "Search",
 
   data: () => ({
-    isEditing: false
-  })
+    descriptionLimit: 60,
+    entries: [],
+    isLoading: false,
+    model: null,
+    search: null
+  }),
+
+  computed: {
+    fields() {
+      if (!this.model) return [];
+
+      return Object.keys(this.model).map(key => {
+        return {
+          key,
+          value: this.model[key] || "n/a"
+        };
+      });
+    },
+    items() {
+      return this.entries.map(entry => {
+        const Description =
+          entry.Description.length > this.descriptionLimit
+            ? entry.Description.slice(0, this.descriptionLimit) + "..."
+            : entry.Description;
+
+        return Object.assign({}, entry, { Description });
+      });
+    }
+  },
+
+  watch: {
+    search() {
+      // Items have already been loaded
+      if (this.items.length > 0) return;
+
+      // Items have already been requested
+      if (this.isLoading) return;
+
+      this.isLoading = true;
+
+      // Lazily load input items
+      fetch("https://api.publicapis.org/entries")
+        .then(res => res.json())
+        .then(res => {
+          const { count, entries } = res;
+          this.count = count;
+          this.entries = entries;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(() => (this.isLoading = false));
+    }
+  }
 };
 </script>
 
 <style>
 .v-autocomplete__content.v-menu__content {
+  transform-origin: center top !important;
+  transform: scale(0.95) !important;
+}
+.v-progress-linear {
   transform-origin: center top !important;
   transform: scale(0.95) !important;
 }
