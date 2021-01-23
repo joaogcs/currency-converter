4eeec68bdd87b6922eec405cd030a4265fac2f3d
João <joaogcsoares1@gmail.com>
2021-01-23 02:26:30 -0300

    Making search usable

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,41 +1,84 @@
 <template>
   <v-container>
     <v-progress-linear
-      :active="true"
-      :indeterminate="true"
+      :active="isLoading"
+      :indeterminate="isLoading"
       color="secondary"
       rounded
     ></v-progress-linear>
     <v-autocomplete
       v-model="model"
       :items="items"
+      @input="search = null"
       :search-input.sync="search"
-      class="mb-n7"
+      item-color="secondary"
+      hide-details="auto"
       hide-no-data
       hide-selected
       label="Search currencies"
       placeholder="Start typing to search"
-      append-icon="mdi-magnify"
+      prepend-inner-icon="mdi-magnify"
+      :menu-props="{
+        closeOnClick: false,
+        closeOnContentClick: false,
+        disableKeys: false,
+        openOnClick: false,
+        maxHeight: 304,
+        offsetY: true,
+        // rounded: 'b-xl',
+        offsetOverflow: false,
+        transition: 'slide-y-transition'
+      }"
       auto-select-first
       chips
       clearable
       deletable-chips
       filled
       multiple
-      rounded
       solo
+      :filter="filterObject"
+      item-text="id"
       return-object
-    ></v-autocomplete>
-    <v-expand-transition>
-      <v-list v-if="model" class="red lighten-3">
-        <v-list-item v-for="(field, i) in fields" :key="i">
-          <v-list-item-content>
-            <v-list-item-title v-text="field.value"></v-list-item-title>
-            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
-          </v-list-item-content>
+    >
+      <template v-slot:no-data>
+        <v-list-item>
+          <v-list-item-title>
+            Search for your favorite
+            <strong>Currency</strong>
+          </v-list-item-title>
         </v-list-item>
-      </v-list>
-    </v-expand-transition>
+      </template>
+      <template v-slot:selection="{ attr, on, item, selected }">
+        <v-chip
+          close
+          v-bind="attr"
+          :input-value="selected"
+          color="accent"
+          v-on="on"
+          @click:close="remove(item.id)"
+        >
+          <v-icon left> mdi-currency-{{ item.id.toLowerCase() }} </v-icon>
+          <span v-text="item.id"></span>
+        </v-chip>
+      </template>
+      <template v-slot:item="{ item }">
+        <v-list-item-avatar
+          color="secondary"
+          class="headline font-weight-light"
+        >
+          {{ item.id.charAt(0) }}
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.id"></v-list-item-title>
+          <v-list-item-subtitle
+            v-text="item.currencyName"
+          ></v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-icon>mdi-currency-{{ item.id.toLowerCase() }}</v-icon>
+        </v-list-item-action>
+      </template></v-autocomplete
+    >
   </v-container>
 </template>
 
@@ -44,32 +87,37 @@ export default {
   name: "Search",
 
   data: () => ({
-    descriptionLimit: 60,
-    entries: [],
+    currencyNameLimit: 60,
     isLoading: false,
+    entries: [],
     model: null,
-    search: null
+    search: null,
+    tab: null
   }),
 
-  computed: {
-    fields() {
-      if (!this.model) return [];
-
-      return Object.keys(this.model).map(key => {
-        return {
-          key,
-          value: this.model[key] || "n/a"
-        };
-      });
+  methods: {
+    remove(id) {
+      this.entries = this.entries.filter(
+        entry => entry.id.toLowerCase() !== id.toLowerCase()
+      );
     },
+    filterObject(item, queryText) {
+      return (
+        item.id.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) >
+          -1 ||
+        item.currencyName
+          .toLocaleLowerCase()
+          .indexOf(queryText.toLocaleLowerCase()) > -1
+      );
+    }
+  },
+
+  computed: {
     items() {
       return this.entries.map(entry => {
-        const Description =
-          entry.Description.length > this.descriptionLimit
-            ? entry.Description.slice(0, this.descriptionLimit) + "..."
-            : entry.Description;
+        const id = entry.id;
 
-        return Object.assign({}, entry, { Description });
+        return Object.assign({}, entry, { id });
       });
     }
   },
@@ -79,18 +127,15 @@ export default {
       // Items have already been loaded
       if (this.items.length > 0) return;
 
-      // Items have already been requested
-      if (this.isLoading) return;
-
       this.isLoading = true;
 
       // Lazily load input items
-      fetch("https://api.publicapis.org/entries")
-        .then(res => res.json())
+      fetch(
+        `https://free.currconv.com/api/v7/currencies?apiKey=d8b2e9d59ba3d57fba43`
+      )
+        .then(res => res.clone().json())
         .then(res => {
-          const { count, entries } = res;
-          this.count = count;
-          this.entries = entries;
+          this.entries = Object.keys(res.results).map(key => res.results[key]);
         })
         .catch(err => {
           console.log(err);
@@ -104,10 +149,10 @@ export default {
 <style>
 .v-autocomplete__content.v-menu__content {
   transform-origin: center top !important;
-  transform: scale(0.95) !important;
+  /* transform: scale(0.95) !important; */
 }
 .v-progress-linear {
   transform-origin: center top !important;
-  transform: scale(0.95) !important;
+  /* transform: scale(0.95) !important; */
 }
 </style>
