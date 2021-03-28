fc199dd3af39b141138af8fc8f86cdc8d76cdf4b
Joao Soares <joao.soares@enext.com.br>
2021-03-28 16:10:23 -0300

    optimize: adding repository link to description

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,25 @@
 <template>
   <v-container id="About" class="about">
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-    </v-row>
-    <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           About
         </h1>
 
+        <h4>Made with Vuetify</h4>
         <p class="subheading font-weight-regular">
+          Github repository
+          <a
+            href="https://github.com/joaogcs/vuetify-currency-converter"
+            target="_blank"
+            >vuetify-currency-converter</a
+          >
+        </p>
+        <p class="subheading font-weight-regular">
+          <br />
           For help and collaboration with other Vuetify developers,
-          <br />please join our online
+          <br />
+          please join our online
           <a href="https://community.vuetifyjs.com" target="_blank"
             >Discord Community</a
           >
