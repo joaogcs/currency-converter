b2004033409d0b5f4fc3eabe60633f480d3353b9
Joao Soares <joao.soares@enext.com.br>
2021-03-28 16:10:45 -0300

    optimize: adding title

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="Details" class="pa-6">
-    Add more details here
-  </div>
+  <v-container id="Details" class="details">
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Details
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          You can add here any extra information about currencies.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
