0413b40d0fc8ce1be12ae7a2487943c3b24b75eb
Joao Soares <joao.soares@enext.com.br>
2021-03-29 22:06:53 -0300

    gh-pages

---
+++ precache-manifest.10bd65e159c2c283505ea0ecc5ab2982.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a61f046f14a30fcac1",
+    "url": "/vuetify-currency-converter/css/app.aa979d6b.css"
+  },
+  {
+    "revision": "7075e9e58d216828d7d7",
+    "url": "/vuetify-currency-converter/css/chunk-vendors.b729a051.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/vuetify-currency-converter/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/vuetify-currency-converter/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/vuetify-currency-converter/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/vuetify-currency-converter/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "ef57797bff406d2c99c0fc85746fea0e",
+    "url": "/vuetify-currency-converter/index.html"
+  },
+  {
+    "revision": "8bbcbc06713f8b960d4a",
+    "url": "/vuetify-currency-converter/js/about.3fec5103.js"
+  },
+  {
+    "revision": "38a61f046f14a30fcac1",
+    "url": "/vuetify-currency-converter/js/app.bcdb1c20.js"
+  },
+  {
+    "revision": "7075e9e58d216828d7d7",
+    "url": "/vuetify-currency-converter/js/chunk-vendors.0e3591e7.js"
+  },
+  {
+    "revision": "85f42207bc328c1ab4956db9e0236ed3",
+    "url": "/vuetify-currency-converter/manifest.json"
+  }
+]);
